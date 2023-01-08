6656142825275cdc6b2de33e69e8a0daeafd5e7a
Kirill Kovalev <kir017@mail.ru>
2023-01-08 18:17:46 +0300

    Нарисована блок схема и написан код к третьей задаче

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (1 <= number&& number <= 7)
+{
+    if (number > 5)
+    {
+      Console.WriteLine(" Ура, ВЫХОДНОЙ!!! ");
+    }
+    else
+    {
+       Console.WriteLine(" Это рабочий день.");
+    }
+
+}
+else
+{
+   Console.WriteLine(" Вы ввели не верные данные. поддерживается ввод числа от 1 до 7");
+}
