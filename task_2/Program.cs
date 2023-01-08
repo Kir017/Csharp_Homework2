a09ffeaaa6c067a32d9df8935f8ab7c67b739d7e
Kirill Kovalev <kir017@mail.ru>
2023-01-08 18:12:52 +0300

    Нарисована блок схема и написан код ко второй задаче

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number >= 1000)
+{
+  number /= 10;
+}
+
+if (number < 100)
+{
+  Console.WriteLine("Третьей цифры нет!!!");
+
+}
+else
+{
+    number = number % 10;
+    Console.WriteLine(number);
+}
+
