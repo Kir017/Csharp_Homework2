ec2d9ff5d3bb140981e1e13326081c7039456b95
Kirill Kovalev <kir017@mail.ru>
2023-01-15 20:22:27 +0300

    поправил код

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -3,10 +3,10 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (100 <= number && number <= 999)
 {
   number /= 10;
-  number = number % 10;
+  number %= 10;
   Console.WriteLine(number);
 }
 else
 {
-Console.WriteLine("Нужно ввести трехзначное число");
+  Console.WriteLine("Нужно ввести трехзначное число");
 }
--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,18 +1,18 @@
 ﻿Console.Write("Введите число дня недели");
 int number = Convert.ToInt32(Console.ReadLine());
-if (1 <= number&& number <= 7)
+if (1 <= number && number <= 7)
 {
-    if (number > 5)
-    {
-      Console.WriteLine(" Ура, ВЫХОДНОЙ!!! ");
-    }
-    else
-    {
-       Console.WriteLine(" Это рабочий день.");
-    }
+  if (number > 5)
+  {
+    Console.WriteLine(" Ура, ВЫХОДНОЙ!!! ");
+  }
+  else
+  {
+    Console.WriteLine(" Это рабочий день.");
+  }
 
 }
 else
 {
-   Console.WriteLine(" Вы ввели не верные данные. поддерживается ввод числа от 1 до 7");
+  Console.WriteLine(" Вы ввели не верные данные. поддерживается ввод числа от 1 до 7");
 }
